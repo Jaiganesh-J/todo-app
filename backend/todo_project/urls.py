9334ac68ff53f4a501d293ebc368f89b054efdcc
Jaiganesh-J <jaig570@gmail.com>
2024-11-06 11:30:31 +0530

    Create serializers.py and update views.py

--- backend/todo_project/urls.py
+++ backend/todo_project/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include(todos.urls)),
+    path("api/", include("todos.urls")),
 ]
--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import ListTodo, DetailTodo
 
-urlpatterns - [
-    path("<int:pk0\>/", DetailTodo.as_view()),
+urlpatterns = [
+    path("<int:pk>", DetailTodo.as_view()),
     path("", ListTodo.as_view()),
 ]
